db686bf7fdb16e007c457e90f3103e7436c6b49a
Xu Yao <clearbamboo@outlook.com>
2024-05-06 22:14:06 +0800

    修改script

--- example_main_script.sh
+++ example_main_script.sh
@@ -1,9 +1,9 @@
 ############## Full Evaluate ###################
 # Train in 35 datasets using GNN in Multi-task (Scaffold Split)
-CUDA_VISIBLE_DEVICES=0 python main.py --task allseed-35ds-scaf-60ex-gnn --model TRMoCE --device cuda --epoch 100 --num_experts 60 --emb_dim 300 --lr 1e-2 --eta_min 1e-8 --seed 0 --k 4 --emb_desc --dataset cls --drop_ratio 0.3 --num_layer 4 --full_evaluate --full_eval_step 1 --batch_size 512 --num_g_experts 6 --use_valid --train_times 10 --sag_pool --open_dy --iattvec_loss
+CUDA_VISIBLE_DEVICES=0 python main.py --task allseed-35ds-scaf-60ex-gnn --model GNN --device cuda --epoch 100 --emb_dim 1024 --lr 1e-2 --eta_min 1e-8 --seed 0 --train_times 10 --dataset cls --dataset cls --drop_ratio 0.3 --use_valid --full_evaluate --num_layer 4 --full_eval_step 1 --batch_size 512  --JK last --graph_pooling sum --gnn_type gin
 
 # Train in 35 datasets using MoE-GNN in Multi-task (Scaffold Split)
-CUDA_VISIBLE_DEVICES=0 python main.py --task allseed-35ds-scaf-60ex-moe --model TRMoCE --device cuda --epoch 100 --num_experts 60 --emb_dim 300 --lr 1e-2 --eta_min 1e-8 --seed 0 --k 4 --emb_desc --dataset cls --drop_ratio 0.3 --num_layer 4 --full_evaluate --full_eval_step 1 --batch_size 512 --num_g_experts 6 --use_valid --train_times 10 --sag_pool --open_dy --iattvec_loss
+CUDA_VISIBLE_DEVICES=0 python main.py --task allseed-35ds-scaf-60ex-moe --model TRMoCE --device cuda --epoch 100 --num_experts 60 --emb_dim 300 --lr 1e-2 --eta_min 1e-8 --seed 0 --k 4 --emb_desc --dataset cls --drop_ratio 0.3 --num_layer 4 --full_evaluate --full_eval_step 1 --batch_size 512 --num_g_experts 6 --use_valid --train_times 10
 
 # Train in 35 datasets using MoCE-GNN in Multi-task (Scaffold Split)
 CUDA_VISIBLE_DEVICES=0 python main.py --task allseed-35ds-scaf-60ex-moce --model TRMoCE --device cuda --epoch 100 --num_experts 60 --emb_dim 300 --lr 1e-2 --eta_min 1e-8 --seed 0 --k 4 --emb_desc --dataset cls --drop_ratio 0.3 --num_layer 4 --full_evaluate --full_eval_step 1 --batch_size 512 --num_g_experts 6 --use_valid --train_times 10 --sag_pool --open_dy --iattvec_loss
